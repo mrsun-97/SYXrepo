36dc3d71f5df8493465a77e65a4b2bc9b59b9325
syx <syx6@mail.ustc.edu.cn>
2018-06-17 20:13:38 +0800

    finish ode45_{rk4,adams3}

--- rs_projects/ode45/src/lib.rs
+++ rs_projects/ode45/src/lib.rs
@@ -12,25 +12,25 @@ pub mod solve{
             else {return vec!(y0);}
         };
         let mut y: Vec<f64> = Vec::with_capacity(n+1);
-        let mut k: [f64;4] = [0.0,0.0,0.0,0.0];
+        let mut k: [f64;5] = [0.0;5];
         y.push(y0);
         let mut xn: f64 = a;
-        let mut _solve = |xn, yn| {
+        let mut _solve = |xn, yn, h| {
             k[1] = f(xn,yn);
             k[2] = f(xn+0.5*h, yn+0.5*h*k[1]);
             k[3] = f(xn+0.5*h, yn+0.5*h*k[2]);
             k[4] = f(xn+h, yn+h*k[3]);
-            yn+h/6.0*(k[1]+k[2]+k[3]+k[4])
+            yn+h/6.0*(1.0*k[1]+2.0*k[2]+2.0*k[3]+1.0*k[4])
         };
         for i in 1..=n {
             xn = a + h*(i as f64);
             let yn = y[i-1];
-            y.push(_solve(xn-h,yn));
+            y.push(_solve(xn-h,yn,h));
         }
         if (xn-b).abs() >= DELTA {
             let h = b-xn;
             let yn = y[n];
-            y.push(_solve(xn,yn));
+            y.push(_solve(xn,yn,h));
         }
         y
     }
@@ -45,20 +45,20 @@ pub mod solve{
             else if b < a {-h.abs()}
             else {return vec!(y0);}
         };
-        //let mut y: Vec<f64> = Vec::with_capacity(n+1);
+
         //起步计算
         let mut y = rk4(f, a, y0, a+2.0*h, h);
         if y.len() != 3 {panic!("error")};
         y.reserve(n-2);
+        //println!("{}", y.len());
         let mut ff: Vec<f64> = Vec::with_capacity(n-1);
         for i in 0..2 {
             ff.push(f(a+h*(i as f64),y[i]));
         }
         let mut xn: f64 = 0.0;
-        
         for i in 3..=n {
             xn = a + h*(i as f64);
-            let fu = f(xn-h,y[n-1]);
+            let fu = f(xn-h,y[i-1]);
             ff.push(fu);
             let mut ybar = y[i-1] + h/12.0*(23.0*ff[i-1] - 16.0*ff[i-2] + 5.0*ff[i-3]);
             ybar = y[i-1] + h/12.0*(5.0*f(xn,ybar) +8.0*ff[i-1] - 1.0*ff[i-2]);
@@ -67,7 +67,10 @@ pub mod solve{
         if (xn-b).abs() >= DELTA {      //收尾
             let yn = y[n];
             let end = rk4(f, xn, yn, b, h);
-            y.push(end[0]);
+            if end.len() == 2 {
+                y.push(end[1]);
+            }
+            else {panic!("Error occurs at the end of calculating!");}
         }
         y
     }
@@ -78,6 +81,6 @@ pub mod solve{
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(3.6.floor(), 3.0);
+        assert_eq!(1.0+2.0, 3.0);
     }
 }
---
+++ rs_projects/ode45/src/main.rs
@@ -0,0 +1,38 @@
+extern crate ode45;
+use ode45::solve;
+
+fn main(){
+    
+    let f = |x:f64,y:f64| { -(x*y)*(x*y) };
+    let yreal = |x:f64| {3.0/(1.0 + x*x*x)};
+    /*
+    let f = |x:f64,y:f64| { 2.0*x*(x-y) + 1.0 };
+    let yreal = |x:f64| { 3.0*(-x*x).exp() + x };
+    */
+    let mut E: Vec<f64> = vec!();
+    let h: f64 = 0.1;
+    let b: f64 = 1.5;
+    for i in 0..=3 {
+        let k = 2.0_f64.powf(i as f64);
+        let mut y = solve::rk4(&f, 0.0, 3.0, b, h/k);
+        if let Some(e) = y.pop() {
+            let e: f64 = (e - yreal(b)).abs();
+            E.push(e);
+            let o: f64 = (E[0]/E[i]).ln()/k.ln();
+            println!("步长: {:<.4} , {:<20.14e} , {:<20.14e}", h/k, e, o);
+        }
+    }
+    print!("\n");
+    E.truncate(0);
+    for i in 0..=3 {
+        let k = 2.0_f64.powf(i as f64);
+        let mut y = solve::adams(&f, 0.0, 3.0, b, h/k);
+        if let Some(e) = y.pop() {
+            let e: f64 = (e - yreal(b)).abs();
+            E.push(e);
+            let o: f64 = (E[0]/E[i]).ln()/k.ln();
+            println!("步长: {:<.4} , {:<20.14e} , {:<20.14e}", h/k, e, o);
+        }
+    }
+    
+}
