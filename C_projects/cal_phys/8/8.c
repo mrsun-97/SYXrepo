567030ba437334136519ce8fb4fd21f324baddcb
syx <syx6@mail.ustc.edu.cn>
2018-11-12 01:09:14 +0800

    work8 finished

--- C_projects/cal_phys/8/8.c
+++ C_projects/cal_phys/8/8.c
@@ -1,30 +1,40 @@
 #include "prelude.h"
 
-
-
 //512×512 array
 static lattice A;
 
 int main(){
+    FILE *fp=fopen("lattice.txt","w");
+    if(!fp){
+        printf("failed to open lattice.txt");
+        return 2;
+    }
     int i, j, k, tmp;
     int count;
-    const int time = 9012;
-    srand(59);
+    //迭代次数
+    const int time = 101011;
+    srand(17);
     count = 0;
     const int percent = 5;
+    //按给定概率设置初态
     for(i=0;i<LEN;i++){
         for(j=0;j<LEN;j++){
+            rand();
             k = rand()%100;
             if(k<percent){
                 A[i][j] = 1;
-                count++;
             }
             else{
                 A[i][j] = 0;
             }
         }
     }
+
+    //统计位于|1>态的个数
+    count = fcount(A);
     printf("start:\t%.3f\t%%\n",rate(count));
+    //这里输出到了stdout，因为可以通过shell控制重定向到文件
+    
     for(k=0;k<time;k++){
         #pragma omp parallel num_threads(6) threadprivate(tmp)
         {   
@@ -41,7 +51,8 @@ int main(){
                     }
                 }
             }
-            #pragma omp barrier     //貌似自带阻塞不需要这条？
+            #pragma omp barrier     //自带阻塞否？
+
             //更新奇数格子
             #pragma omp for schedule(static, 4)
             for(i=0;i<LEN;i++){
@@ -57,10 +68,20 @@ int main(){
             }
             
         }//end parallel
-        if(k%5==1){
+        if(1){
             count = fcount(A);
             printf("%d\t%.1f\t%%\n",k, rate(count));
         }
     }
+
+    //输出512×512矩阵
+    for(i=0;i<LEN;i++){
+        for(j=0;j<LEN;j++){
+            fprintf(fp, "%d\t", (int)A[i][j]);
+        }
+        fseek(fp, -1L, SEEK_CUR);
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
     return 0;
 }
--- C_projects/cal_phys/8/autorun.sh
+++ C_projects/cal_phys/8/autorun.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
 gcc 8.c -O3 -o 8.out && ./8.out > data0.txt
-ls -lh data?.txt
+ls -lh data0.txt lattice.txt
 
